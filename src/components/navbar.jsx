11244a4e0277b5bc5c0a97c9dbc032e1187888cc
Chloe Thies <chloe.r.thies@gmail.com>
2022-02-09 15:23:35 -0800

    navbar + bootstrap

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#groups">Groups</Nav.Link>
+            <Nav.Link href="#messages">Messages</Nav.Link>
+            <Nav.Link href="#profile">Profile</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default NavBar;
